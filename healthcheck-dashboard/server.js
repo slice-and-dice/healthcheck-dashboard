7551b902191851652029e7a75529780dda25122c
A. P. Malde <anisha.malde@ibm.com>
2018-10-11 16:36:47 +0100

    deploy

--- healthcheck-dashboard/server.js
+++ healthcheck-dashboard/server.js
@@ -16,12 +16,4 @@ app.get("/detailed/:env", async (req, res) => {
   res.send(response);
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   // Serve any static files
-//   app.use(express.static(path.join(__dirname, "client/build")));
-//   // Handle React routing, return all requests to React app
-//   app.get("*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
-//   });
-// }
 app.listen(port, () => console.log(`Listening on port ${port}`));
